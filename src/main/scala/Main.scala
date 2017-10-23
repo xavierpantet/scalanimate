f24c53d36fbd4cf2f1df5e1c441a33dca077b6a3
Xavier Pantet <xavpantet@gmail.com>
2017-10-23 10:05:15 +0200

    Corrected contains method of Rectangles to take rotation into account

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,30 +14,12 @@ object Main {
     implicit val canvas = Canvas("canvas", width, height)
 
     val c1 = Rectangle(width / 2, height / 2, 100, 200)
-    c1.fillColor = Color.red
-    c1.onMouseDown = () => c1.hide()
-    c1.onMouseUp = () => c1.show()
 
-    val c2 = Circle(width / 2, height / 2, 50)
-    c2.fillColor = Color.yellow
-
-    val c3 = Circle(400, 0, 10)
-    c3.fillColor = Color.orange
-
-    every(20.milliseconds) { t => {
-      c1.pointTowards(c2)
-      c3.moveTo(400*Math.cos(Angle(t.toDouble/50).toRadians), 400*Math.sin(Angle(t.toDouble/50).toRadians))
-
-      if (!canvas.keysDown.contains("Shift")) {
-        c1.x = canvas.mouseX
-        c1.y = canvas.mouseY
-        c2.moveTo(1.5*canvas.mouseX, 1.5*canvas.mouseY)
-      }
-      else {
-        c1.x = width / 2
-        c1.y = height / 2
-      }
-    }
-    }
+    c1.turnLeft(20)
+    every(20.milliseconds) { _ => {
+      c1.fillColor = Color.red
+      c1.onMouseDown = () => c1.hide()
+      c1.onMouseUp = () => c1.show()
+    }}
   }
 }
--- src/main/scala/scalanimate/shapes/mutable/Rectangle.scala
+++ src/main/scala/scalanimate/shapes/mutable/Rectangle.scala
@@ -7,6 +7,58 @@ import scalanimate.Canvas
 case class Rectangle(override var x: Double, override var y: Double, var width: Double, var height: Double)(implicit override val canvas: Canvas) extends MutableShape {
 
   /**
+    * Describes the rotation of the shape with respect to its width
+    * @return a pair containing the x and y values of the rotation
+    */
+  private def xRotationTransform = (height*Math.cos(angle.toRadians), height*Math.sin(angle.toRadians))
+
+  /**
+    * Describes the rotation of the shape with respect to its height
+    * @return a pair containing the x and y values of the rotation
+    */
+  private def yRotationTransform = (width*Math.cos((angle-Angle.degrees(90)).toRadians), width*Math.sin((angle-Angle.degrees(90)).toRadians))
+
+  /**
+    * Returns the top right corner of the rectangle
+    * @return (x, y) coordinates of the top right corner
+    */
+  private def bottomRightCorner = (x + xRotationTransform._1, y + xRotationTransform._2)
+
+  /**
+    * Returns the bottom left corner of the rectangle
+    * @return (x, y) coordinates of the bottom left corner
+    */
+  private def topLeftCorner = (x + yRotationTransform._1, y + yRotationTransform._2)
+
+  /**
+    * Returns the bottom right corner of the rectangle
+    * @return (x, y) coordinates of the bottom right corner
+    */
+  private def topRightCorner = (topLeftCorner._1 + xRotationTransform._1, topLeftCorner._2 + xRotationTransform._2)
+
+  /**
+    * Defined the path to draw the rectangle onto the canvas
+    * without actually printing it
+    */
+  private def getPath = {
+    canvas.context.beginPath
+    canvas.context.moveTo(x, y)
+
+    // Draw the bottom line
+    canvas.context.lineTo(bottomRightCorner._1, bottomRightCorner._2)
+
+    // Draw the right line
+    canvas.context.lineTo(bottomRightCorner._1 + yRotationTransform._1, bottomRightCorner._2 + yRotationTransform._2)
+    canvas.context.moveTo(x, y)
+
+    // Draw the left line
+    canvas.context.lineTo(topLeftCorner._1, topLeftCorner._2)
+
+    // Draw the top line
+    canvas.context.lineTo(topRightCorner._1, topRightCorner._2)
+  }
+
+  /**
     * Returns the position of the center of the shape
     * @return the position of the center of the shape
     */
@@ -17,19 +69,7 @@ case class Rectangle(override var x: Double, override var y: Double, var width: 
     * @param canvas the canvas to draw on
     */
   override def draw(implicit canvas: Canvas): Unit = {
-    val xRotationTransform = (height*Math.cos(angle.toRadians), height*Math.sin(angle.toRadians))
-    val yRotationTransform = (width*Math.cos((angle-Angle.degrees(90)).toRadians), width*Math.sin((angle-Angle.degrees(90)).toRadians))
-    val topRightCorner = (x + xRotationTransform._1, y + xRotationTransform._2)
-    val bottomLeftCorner = (x + yRotationTransform._1, y + yRotationTransform._2)
-    val bottomRightCorner = (bottomLeftCorner._1 + xRotationTransform._1, bottomLeftCorner._2 + xRotationTransform._2)
-
-    canvas.context.beginPath
-    canvas.context.moveTo(x, y)
-    canvas.context.lineTo(topRightCorner._1, topRightCorner._2)
-    canvas.context.lineTo(topRightCorner._1 + yRotationTransform._1, topRightCorner._2 + yRotationTransform._2)
-    canvas.context.moveTo(x, y)
-    canvas.context.lineTo(bottomLeftCorner._1, bottomLeftCorner._2)
-    canvas.context.lineTo(bottomRightCorner._1, bottomRightCorner._2)
+    getPath
     canvas.context.fillStyle = fillColor.toCanvas
     canvas.context.fill
     canvas.context.strokeStyle = borderColor.toCanvas
@@ -41,7 +81,10 @@ case class Rectangle(override var x: Double, override var y: Double, var width: 
     * @param x the x coordinate of the point
     * @param y the y coordinate of the point
     */
-  override def contains(x: Double, y: Double): Boolean = (x >= this.x && x <= this.x + width) && (y >= this.y && y <= this.y + height)
+  override def contains(x: Double, y: Double): Boolean = {
+    getPath
+    canvas.context.isPointInPath(x, y)
+  }
 
 }
 
