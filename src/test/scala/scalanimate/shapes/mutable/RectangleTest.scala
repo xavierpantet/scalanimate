2f092b890f45d3a7a1f22bfbee3b37a15d304637
Xavier Pantet <xavpantet@gmail.com>
2017-11-13 14:27:24 +0100

    Created first test for Rectangles

--- src/test/scala/scalanimate/shapes/mutable/RectangleTest.scala
+++ src/test/scala/scalanimate/shapes/mutable/RectangleTest.scala
@@ -1,11 +1,13 @@
 package scalanimate.shapes.mutable
 
+import org.scalajs.dom.document
+import org.scalajs.dom.raw.HTMLCanvasElement
 import org.scalatest.FunSpec
 
 import scalanimate.Canvas
 
 class RectangleTest extends FunSpec {
-  implicit val c = Canvas("test", 100, 100)
+  implicit val c = Canvas(document.createElement("canvas").asInstanceOf[HTMLCanvasElement], 100, 100)
   val aligned = Rectangle(100, 100, 50, 50)
   val rotated = Rectangle(100, 100, 50, 50)
   rotated.turnLeft(30)
