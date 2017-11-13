223634d2c278ce23ce094e738620b88ede5fde4b
Xavier Pantet <xavpantet@gmail.com>
2017-11-13 15:00:51 +0100

    More tests for rectangles

--- src/test/scala/scalanimate/shapes/mutable/RectangleTest.scala
+++ src/test/scala/scalanimate/shapes/mutable/RectangleTest.scala
@@ -5,6 +5,7 @@ import org.scalajs.dom.raw.HTMLCanvasElement
 import org.scalatest.FunSpec
 
 import scalanimate.Canvas
+import scalanimate.core.Vector2D
 
 class RectangleTest extends FunSpec {
   implicit val c = Canvas(document.createElement("canvas").asInstanceOf[HTMLCanvasElement], 100, 100)
@@ -12,9 +13,32 @@ class RectangleTest extends FunSpec {
   val rotated = Rectangle(100, 100, 50, 50)
   rotated.turnLeft(30)
 
-  describe("Rectangles"){
-    it("Should return the correct edges vector"){
-      assert(aligned.getVectorsFromCenterToEveryCorner.size == 4)
+  describe("A rectangle"){
+    it("Should return the correct edges vector when aligned"){
+      val testedVectors = aligned.getVectorsFromCenterToEveryCorner
+
+      assert(testedVectors.distinct.size == 4)
+
+      val expectedVectors = List(Vector2D(100, 100), Vector2D(150, 100), Vector2D(100, 50), Vector2D(150, 50))
+      assert(testedVectors.forall(v => expectedVectors.exists(_ == v)))
     }
+  }
+
+  it("Should return the correct axis when aligned"){
+    val testedAxis = aligned.getNormalEdgesVectors
+
+    assert(testedAxis.distinct.size == 4)
+
+    val expectedAxis = List(Vector2D(0, 1), Vector2D(1, 0), Vector2D(0, -1), Vector2D(-1, 0))
+    assert(testedAxis.forall(v => expectedAxis.exists(_ == v)))
+  }
+
+  it("Should return the correct axis when rotated"){
+    val testedAxis = rotated.getNormalEdgesVectors
+
+    assert(testedAxis.distinct.size == 4)
+
+    val expectedAcxis = List(Vector2D(0.5, Math.sqrt(3)/2), Vector2D(0.5, Math.sqrt(3)/2).normal, Vector2D(-0.5, Math.sqrt(3)/2), Vector2D(-0.5, Math.sqrt(3)/2).normal)
+    assert(testedAxis.forall(v => testedAxis.exists(_ == v)))
   }
 }
