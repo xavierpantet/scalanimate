5fd73b959a57266ef0ce15550a50a88301c535f0
Xavier Pantet <xavpantet@gmail.com>
2017-11-15 17:06:35 +0100

    New Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import doodle.core.{Angle, Color}
 
 import scala.concurrent.duration._
 import scalanimate.Canvas
-import scalanimate.shapes.mutable.Circle
+import scalanimate.shapes.mutable.{Circle, Polygon, Rectangle}
 import scalanimate.core.TimeControls._
 
 
@@ -13,10 +13,8 @@ object Main {
     val height = 600
     implicit val canvas = Canvas("canvas", width, height)
 
-    val p = Circle(width/2, height/2, 100)
-    val c = Circle(width/2, height/2, 50)
-
-    //p.turnLeft(37)
+    val c = Circle(width/2, height/2, 100)
+    val p = Circle(width/2, height/2, 50)
 
     every(20.milliseconds){ _ => {
       c.x = canvas.mouseX
@@ -24,9 +22,9 @@ object Main {
       if(c.touches(p)){
         c.fillColor = Color.red
       }
-      /*ele if(c.touchesEdge){
+      else if(c.touchesEdge){
         c.fillColor = Color.blue
-      }*/
+      }
       else{
         c.fillColor = Color.green
       }
