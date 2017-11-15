687bfff0fbf8e08bf003c514711146af9b8b7154
Xavier Pantet <xavpantet@gmail.com>
2017-11-15 17:22:38 +0100

    Updated Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,8 @@
-import doodle.core.{Angle, Color}
+import doodle.core.Color
 
 import scala.concurrent.duration._
 import scalanimate.Canvas
-import scalanimate.shapes.mutable.{Circle, Polygon, Rectangle}
+import scalanimate.shapes.mutable.Polygon
 import scalanimate.core.TimeControls._
 
 
@@ -13,8 +13,8 @@ object Main {
     val height = 600
     implicit val canvas = Canvas("canvas", width, height)
 
-    val c = Circle(width/2, height/2, 100)
-    val p = Circle(width/2, height/2, 50)
+    val c = Polygon(width/2, height/2, 100, 3)
+    val p = Polygon(width/2, height/2, 50, 5)
 
     every(20.milliseconds){ _ => {
       c.x = canvas.mouseX
