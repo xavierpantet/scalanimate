5ed7349d58cc36b32256b813db4c544acd084f5c
Xavier Pantet <xavpantet@gmail.com>
2017-11-18 13:46:54 +0100

    Commented SpaceGameExample

--- src/main/scala/scalanimate/examples/SpaceGameExample.scala
+++ src/main/scala/scalanimate/examples/SpaceGameExample.scala
@@ -9,16 +9,23 @@ import scalanimate.core.TimeControls._
 
 object SpaceGameExample extends Example {
   override def run: Unit = {
+
+    // Set a background
     Image(Background.Space, 0, 0, width, height)
+
+    // Set up rocket and a dot + number of points
     val spaceCraft = Image(Image.Rocket, width/2, height, 80, 50)
     var dot = getRandomCircle
     var points = 0
 
     every(20.milliseconds){_ => {
+      
+      // Update position of the rocket + its direction
       spaceCraft.x = canvas.mouseX
       spaceCraft.y = canvas.mouseY
       spaceCraft.pointTowards(dot)
 
+      // When the rocket touches the dot, increase the number of points + generate new point
       if(spaceCraft.touches(dot)){
         points = points + 1
         println("You have " + points + " points!")
