b9280ac6b5a8c01edb802072e66b947f03a718b4
Xavier Pantet <xavpantet@gmail.com>
2017-11-22 13:59:49 +0100

    Few changes

--- src/main/scala/scalanimate/examples/SimplePongExample.scala
+++ src/main/scala/scalanimate/examples/SimplePongExample.scala
@@ -11,14 +11,21 @@ import scala.scalajs.js.Dynamic.global
 
 object SimplePongExample extends Example {
   override def run: Unit = {
+    // Background
     Image(Background.DarkSpace, 0, 0, width, height)
+
+    // Define the bar
     val bar = Rectangle(width/2 - 50, height, 10, 100)
+
+    // Define the ball + properties
     val ball = Circle(width/2, height-20, 10)
-    var dx = 1 + Random.nextInt(9)
-    var dy = -(1 + Random.nextInt(9))
     ball.fillColor = Color.yellow
+    var (dx, dy) = (1 + Random.nextInt(9), -(1 + Random.nextInt(9)))
+
+    // Game state
     var gamePaused = true
 
+    // GAME
     every(20.milliseconds){_ => {
       if(!gamePaused) {
         manageEdges
@@ -31,6 +38,9 @@ object SimplePongExample extends Example {
         gamePaused = !gamePaused
     }}
 
+    /**
+      * Private method that moves the ball depending on its position
+      */
     def manageEdges: Unit = {
       if(ball.touchesEdge){
         if(ball.y <= 5 || ball.y >= height-5 || ball.touches(bar))
