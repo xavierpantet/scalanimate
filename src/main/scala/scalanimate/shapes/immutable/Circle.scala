91c6635fe6edb838bbb72eb74b87e13126a96bce
Xavier Pantet <xavpantet@gmail.com>
2017-12-26 15:56:57 +0100

    Removed shapes/immutable useless package

--- src/main/scala/scalanimate/shapes/immutable/Circle.scala
+++
@@ -1,7 +0,0 @@
-package scalanimate.shapes.immutable
-
-import doodle.core.Image.circle
-
-case class Circle(r: Double) extends ImmutableShape {
-  override val image = circle(r)
-}
--- src/main/scala/scalanimate/shapes/immutable/ImmutableShape.scala
+++
@@ -1,7 +0,0 @@
-package scalanimate.shapes.immutable
-import scalanimate.shapes.Shape
-import doodle.core.Image
-
-abstract class ImmutableShape extends Shape {
-  val image: Image
-}
